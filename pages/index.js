4e12b648f82851892c67aaf33f2e6e28e9f12480
Junmin De Real <junmindereal@gmail.com>
2020-09-18 14:41:07 +0800

    Spelling corrected

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home () {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-                    Comming Soon
+                    Coming Soon
         </h1>
       </main>
     </div>
